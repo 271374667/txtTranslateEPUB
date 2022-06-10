73b18c4f207f73199aebad57c52478105b6e18f3
bemake <271374667@qq.com>
2022-06-10 18:31:59 +0800

    new version: 第一次提交，目前已经完成智能排序

---
+++ GUI.py
@@ -0,0 +1,80 @@
+'''
+Author: Bemake
+Date: 2022-06-10 17:23:57
+LastEditTime: 2022-06-10 18:16:14
+Description: 如果代码出现问题请联系本人邮箱:271374667@qq.com
+'''
+import logging
+from gooey import Gooey, GooeyParser
+
+# 当前版本号(常量)
+VERSION = 3.2
+
+logging.basicConfig(level= logging.INFO, format=f'%(asctime)s - %(levelname)s- 当前版本-> V{VERSION} <- - %(message)s')
+
+@Gooey(language='chinese',navigation="TABBED",clear_brfore_run=True)
+def GUI():
+    # 新建一个大的parser容器
+    parser = GooeyParser(description='请选择一项排序的方式来进行下一步的操作')
+    
+    # 为分类方式创建一个容器
+    sub = parser.add_subparsers(help='选择你的分类',dest="RESULT")
+    
+    # 智能排序================================================================
+    smartSortTabbed = sub.add_parser('smartSort')
+            
+    # 打开文件夹
+    smartSortTabbed.add_argument('opendir',metavar='打开文件夹目录',help='请在这里选择你需要转换成EPUB的小说目录',widget='DirChooser')
+            
+    # 在互斥分组内添加选项
+    # smartSortTabbed.add_argument('--smart',metavar="智能排序（推荐）",help='使用智能转换，将小说前面自带的章节数字转换成阿拉伯数字再进行排序',action='store_true')
+    # smartSortTabbed.add_argument('--force',metavar="强制排序（稳定）",help='使用自带的爬虫进行爬取的小说，会在小说章节前有█的标志（如果没有标志请不要选择该项）',action='store_true')
+    # smartSortTabbed.add_argument('--default',metavar="系统默认（不推荐）",help='使用系统默认的排序方法，章节的顺序可能会发生错乱\n如果不做选择默认将会选择系统默认',action='store_true')
+    
+    # 增加更多可选项
+    more = smartSortTabbed.add_argument_group('更多可定制的选项',gooey_options={
+        'show_border': True
+    })
+    
+    # 我们的番外放在哪里
+    more.add_argument('--sideStory',metavar='番外权重',help="我们的番外(作者杂谈之类的)放在哪里?",choices=['最前面','最后面'],default='最后面')
+    
+    # 我们每一行需要换几次空格
+    more.add_argument('--space',metavar='空格数量',help='小说每一行之间都会有空行,你可以在这里设置空行的大小, \
+                        如果发现空行过大可以适当调小该值',choices=['0','1','2','3'],default='1')
+    
+    # 对于某些广告或者特殊元素的屏蔽
+    more.add_argument('--filter',metavar="过滤字符串",help="如果你在元素里面发现一些特殊的字符串，并且他们大量出现在你的小说里面 \
+                        那么你可以尝试在这里输入他们来屏蔽这些字符串,比如 <欢迎来到XX小说网> 你也可以同时输入多个值,他们之间用 \\n 隔开",widget="Textarea",
+                        gooey_options={
+                            'height': 100
+                        })
+    
+    # 最终显示我们的图形化界面
+    args = parser.parse_args()
+    
+    # 将我们的参数返回
+    return args
+
+
+# 调用GUI的同时返回变量，方便我们主程序引用
+# Args我们GUI的输出
+Args = GUI()
+print(Args) 
+
+# 番外的权重(默认为10W)
+sideStoryWeight = 100000
+
+# 空格的数量(默认为1)
+spaceNumber = Args.space
+
+# 过滤的关键词(默认为无,默认的分割符为\n)
+filterWord = list(Args.filter.split("\\n")) if Args.filter != None else None
+print(filterWord)
+
+# 新建当前文件夹下所有的文件名
+currentDirAllFileNameList = []
+
+# 新建一个排序过后的文件名字列表
+AllFileSortedList = []
+
---
+++ main.py
@@ -0,0 +1,139 @@
+import logging
+import os
+import os.path
+import re
+import pypandoc
+import cn2an
+import GUI  # 调用我们的GUI界面
+
+# 当前版本号(常量)
+VERSION = 3.2
+
+logging.basicConfig(level=logging.INFO,
+                    format=f'%(asctime)s - %(levelname)s- 当前版本-> V{VERSION} <- - %(message)s')
+
+# 打印函数
+
+
+def printToFile():
+    # 打印次数
+    counter = 0
+
+    # 第一次打印封面
+    booktitle = True
+
+    # 获取当前文件夹的名字
+    novel_title = os.path.basename(os.getcwd())
+
+    for each_file in GUI.AllFileSortedList:
+        # 把章节名字分离
+        print(each_file)
+        name = each_file[1].split('.txt')[0]
+
+        # 打开文件来读取内容
+        with open(name + '.txt', 'r', encoding='utf-8') as f:
+            content = f.readlines()
+
+            # 将每一章的内容写入到我们的大章
+            with open(novel_title + ".txt", 'a+', encoding='utf-8') as f2:
+                counter += 1
+
+                # 使用笨方法来避免重复打印
+                if booktitle == True:
+                    # 封面页的简介
+                    f2.write('% ' + novel_title + '\n')
+                    f2.write('% ' + '该软件由\tBemake\t无偿编写\t请勿二手倒卖,联系邮箱:271374667@qq.com' + '\n\n')
+                    booktitle = False
+
+                f2.write(f'\n#  {name}\n\n')
+                for contents in content:
+                    
+                    # 替换需要过滤的字符串
+                    for i in GUI.filterWord:
+                        contents = contents.replace(i,'')
+                        
+                    # 打印我们的内容
+                    f2.write(contents)
+                    
+                    # 给我们的小说换行
+                    for _ in range(int(GUI.spaceNumber)):
+                        f2.write("\n")
+
+        logging.info(f'目前打印了{counter}次,正在打印\t{each_file}')
+
+    logging.warning('打印结束，正在生成EPUB文件………………')
+
+    # 打印最终的epub文件
+    with open(novel_title + '.txt', 'r', encoding='utf-8') as f3:
+        content = f3.readlines()
+        Strcontent = ''.join(content)
+        pypandoc.convert_text(Strcontent, 'epub', format='md',
+                              outputfile=novel_title + '.epub')
+
+
+def smartSortStart():
+    # 提取我们文本中的数字(提取我们的第一个数字)
+    for eachFile in GUI.currentDirAllFileNameList:
+        
+        # 使用守卫if单独处理我们的废话
+        if eachFile.find('章') == -1 and eachFile.find('第') == -1:
+            # 根据权重
+            if GUI.gooeyArgs.sideStory == '最后面':                 
+                GUI.AllFileSortedList.append((GUI.sideStoryWeight, eachFile))
+            
+            else:
+                GUI.AllFileSortedList.append((GUI.sideStoryWeight - 200000, eachFile))
+            
+            # 确保文件能够递增
+            GUI.sideStoryWeight += 1
+        
+        
+        # 将标题中所有的数字翻译成我们的阿拉伯数字
+        title2number = cn2an.transform(eachFile,'cn2an')
+        
+        # 提取标题中的第一个数字,一直到章为止
+        p = re.compile(r'第(\d+?)章.*?\.txt')
+        title_index = re.findall(p, title2number)
+        
+        # 将提取之后的章节数字和文件的名称组成一个(章节数字，章节名称)元组，方便我们后期来排序
+        if title_index != []:
+            GUI.AllFileSortedList.append((title_index[0], eachFile))
+        
+            logging.debug(f"添加了一个新的数字编号 {title_index} 字")
+                    
+    # 对列表的内容进行排序
+    GUI.AllFileSortedList = sorted(GUI.AllFileSortedList,key=lambda x: int(x[0]))
+    
+    # 调试
+    logging.debug(GUI.AllFileSortedList)
+    
+    # 对数据内容进行打印并排版        
+    printToFile()
+
+
+def main():
+
+    # 切换我们的工作路径到目标文件夹
+    logging.info(GUI.Args)
+    os.chdir(GUI.Args.opendir)
+
+    # 获取当前文件夹下所有的文件
+    GUI.currentDirAllFileNameList = os.listdir('.')
+
+    # 进行选择我们要进行的是哪个操作
+    if GUI.Args.RESULT == 'smartSort':
+        logging.info('当前选择了 smart')
+        
+        # 调用我们的只能排序方法
+        smartSortStart()
+
+    if GUI.Args.RESULT == 'force':
+        logging.info('当前选择了 force')
+
+    if GUI.Args.RESULT == 'default':
+        logging.info('当前选择了 default')
+
+
+if __name__ == '__main__':
+    GUI.GUI()
+    main()
